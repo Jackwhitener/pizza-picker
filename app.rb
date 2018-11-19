beb8d25b36eae9c7c728eeaa360aa8b3e052383a
Jackwhitener <jackwhitener1@gmail.com>
2018-11-19 16:53:17 -0500

    Now asks for pizza size. Mama mia.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require 'sinatra'
 get '/' do
-	
+	erb :start
+end
+post '/start' do
+	redirect '/crust?'
+end
+get '/crust' do
+	erb :crust
+end
+post '/crust' do
+	size = params[:size]
+	redirect '/bread?size=' + size
+end
+get '/bread' do
+
 end
