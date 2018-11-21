798f069707496a59ccd84677e5726c45199a03ad
Jackwhitener <jackwhitener1@gmail.com>
2018-11-20 23:32:31 -0500

    Adds a page for the address, or in-store pickup.

--- app.rb
+++ app.rb
@@ -19,7 +19,7 @@ post '/pizza' do
 	sauce = params[:sauce]
 	cheese = params[:cheese]
 	toppings = params[:toppings]
-	redirect 'verify?size=' + size.to_s + '&bread=' + bread.to_s + '&sauce=' + sauce.to_s + '&cheese=' + cheese.to_s + '&toppings=' + toppings.to_s
+	redirect '/verify?size=' + size.to_s + '&bread=' + bread.to_s + '&sauce=' + sauce.to_s + '&cheese=' + cheese.to_s + '&toppings=' + toppings.to_s
 end
 get '/verify' do
 	size = eval(params[:size])
@@ -28,4 +28,36 @@ get '/verify' do
 	cheese = eval(params[:cheese])
 	toppings = eval(params[:toppings])
 	erb :verify, locals: {size: size, bread: bread, sauce: sauce, cheese: cheese, toppings: toppings}
+end
+post '/verify' do
+	size = eval(params[:size])
+	bread = eval(params[:bread])
+	sauce = eval(params[:sauce])
+	cheese = eval(params[:cheese])
+	toppings = eval(params[:toppings])
+	correct = params[:correct]
+	puts "Is it correct: #{correct}"
+	if correct == 'yes'
+		redirect 'yes?size=' + size.to_s + '&bread=' + bread.to_s + '&sauce=' + sauce.to_s + '&cheese=' + cheese.to_s + '&toppings=' + toppings.to_s
+	elsif correct == 'no'
+		redirect '/pizza?'
+	end
+end
+get '/yes' do
+	size = params[:size]
+	bread = params[:bread]
+	sauce = params[:sauce]
+	cheese = params[:cheese]
+	toppings = params[:toppings]
+erb :yes, locals: {size: size, bread: bread, sauce: sauce, cheese: cheese, toppings: toppings}
+end
+post '/address' do
+	size = params[:size]
+	bread = params[:bread]
+	sauce = params[:sauce]
+	cheese = params[:cheese]
+	toppings = params[:toppings]
+	address = params[:address]
+	instorepickup = params[:instorepickup]
+	redirect '/final?size=' + size.to_s + '&bread=' + bread.to_s + '&sauce=' + sauce.to_s + '&cheese=' + cheese.to_s + '&toppings=' + toppings.to_s + '&address=' + address '&instorepickup=' + instorepickup
 end
