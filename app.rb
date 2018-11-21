dd0d4cf446f7effb2441c043d15015a9c23c7af6
Jackwhitener <jackwhitener1@gmail.com>
2018-11-21 11:05:57 -0500

    Fixes the no topping issue

--- app.rb
+++ app.rb
@@ -18,10 +18,12 @@ post '/pizza' do
 	bread << params[:bread]
 	sauce = params[:sauce]
 	cheese = params[:cheese]
-	toppings = params[:toppings]
 	if toppings == nil
-		toppings << "nothing"
+		toppings = ["nothing"]
+	else
+		toppings = params[:toppings]
 	end
+	puts "toppings: #{toppings}"
 	redirect '/verify?size=' + size.to_s + '&bread=' + bread.to_s + '&sauce=' + sauce.to_s + '&cheese=' + cheese.to_s + '&toppings=' + toppings.to_s
 end
 get '/verify' do
