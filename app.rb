5816de91244ec0ec4401b579fa83b1d43f42bcbe
Jackwhitener <jackwhitener1@gmail.com>
2018-11-24 12:34:33 -0500

    Adds support for multiple pizzas.

--- app.rb
+++ app.rb
@@ -3,65 +3,55 @@ get '/' do
 	erb :start
 end
 post '/start' do
-	redirect '/pizza?'
+	pizza = Array.new
+	redirect '/pizza?pizza=' + pizza.to_s
 end
 get '/pizza' do
-	erb :pizza
+	pizza = params[:pizza]
+	erb :pizza, locals: {pizza: pizza}
 end
 post '/pizza' do
-	size = Array.new
-	bread = Array.new
-	sauce = Array.new
-	cheese = Array.new
-	toppings = Array.new
-	size << params[:size]
-	bread << params[:bread]
-	sauce = params[:sauce]
-	cheese = params[:cheese]
-	toppings = params[:toppings]
-	redirect '/verify?size=' + size.to_s + '&bread=' + bread.to_s + '&sauce=' + sauce.to_s + '&cheese=' + cheese.to_s + '&toppings=' + toppings.to_s
+	pizza = eval(params[:pizza])
+	addedpizza = Array.new
+	addedpizza << params[:size]
+	addedpizza << params[:bread]
+	addedpizza << params[:sauce]
+	addedpizza << params[:cheese]
+	addedpizza << params[:toppings]
+	pizza << addedpizza
+	puts "pizza @ '/pizza': #{pizza}"
+	# pizza.delete_at(0)
+	redirect '/verify?pizza=' + pizza.to_s
 end
 get '/verify' do
-	size = eval(params[:size])
-	bread = eval(params[:bread])
-	sauce = eval(params[:sauce])
-	cheese = eval(params[:cheese])
-	toppings = eval(params[:toppings])
-	erb :verify, locals: {size: size, bread: bread, sauce: sauce, cheese: cheese, toppings: toppings}
+	pizza = eval(params[:pizza])
+	erb :verify, locals: {pizza: pizza}
 end
 post '/verify' do
-	size = eval(params[:size])
-	bread = eval(params[:bread])
-	sauce = eval(params[:sauce])
-	cheese = eval(params[:cheese])
-	toppings = eval(params[:toppings])
-	redirect 'yes?size=' + size.to_s + '&bread=' + bread.to_s + '&sauce=' + sauce.to_s + '&cheese=' + cheese.to_s + '&toppings=' + toppings.to_s
+	puts "pizza @ '/verify'pizza"
+	pizza = params[:pizza]
+	addpizza = params[:addpizza]
+	if addpizza == 'No'
+		redirect '/yes?pizza=' + pizza.to_s
+	elsif addpizza == 'Yes'
+		redirect '/pizza?pizza=' + pizza.to_s
+	end
 end
 get '/yes' do
-	size = params[:size]
-	bread = params[:bread]
-	sauce = params[:sauce]
-	cheese = params[:cheese]
-	toppings = params[:toppings]
-erb :yes, locals: {size: size, bread: bread, sauce: sauce, cheese: cheese, toppings: toppings}
+	pizza = params[:pizza]
+erb :yes, locals: {pizza: pizza}
 end
 post '/address' do
-	size = params[:size]
-	bread = params[:bread]
-	sauce = params[:sauce]
-	cheese = params[:cheese]
-	toppings = params[:toppings]
+	puts "pizza @ '/address'pizza"
+	pizza = params[:pizza]
 	address = params[:address]
 	instorepickup = params[:instorepickup]
-	redirect '/final?size=' + size.to_s + '&bread=' + bread.to_s + '&sauce=' + sauce.to_s + '&cheese=' + cheese.to_s + '&toppings=' + toppings.to_s + '&address=' + address.to_s + '&instorepickup=' + instorepickup
+	redirect '/final?pizza=' + pizza + '&address=' + address.to_s + '&instorepickup=' + instorepickup.to_s
 end
 get '/final' do
-	size = eval(params[:size])
-	bread = eval(params[:bread])
-	sauce = eval(params[:sauce])
-	cheese = eval(params[:cheese])
-	toppings = eval(params[:toppings])
+	pizza = eval(params[:pizza])
+	puts "pizzas: #{pizza}"
 	address = eval(params[:address])
 	instorepickup = params[:instorepickup]
-	erb :final, locals: {size: size, bread: bread, sauce: sauce, cheese: cheese, toppings: toppings, address: address, instorepickup: instorepickup}
+	erb :final, locals: {pizza: pizza, address: address, instorepickup: instorepickup}
 end
